c87afcb1611cddd165ba4f207b45f0a9317e12b6
cequella <pedro.oliveira.demolay@gmail.com>
2017-09-24 14:51:42 -0300

    Creating about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule }      from '@angular/core';
+import { RouterModule }  from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent }       from './app.component';
 import { PortfolioComponent } from './page/portfolio/portfolio.component';
+import { HomeComponent }      from './page/home/home.component';
+import { AboutComponent }     from './page/about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PortfolioComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		PortfolioComponent,
+		HomeComponent,
+		AboutComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot([
+			{path: '',          component: HomeComponent},
+			{path: 'portfolio', component: PortfolioComponent},
+			{path: 'about',     component: AboutComponent},
+		])
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
