4758566c993a8495e3cba83c46543ef557f67c3a
cequella <pedro.oliveira.demolay@gmail.com>
2017-09-27 02:05:32 -0300

    Corrections and project/member style defined

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+	collaborator = [
+		{name: "Zé Mané", job: "Developer",
+		 photo: "https://br.udacity.com/assets/images/nanodegree-overview/shared/nd-instructors/james-williams.jpg",
+		 text: "Mussum Ipsum, cacilds vidis litro abertis. In elementis mé pra quem é amistosis quis leo. Quem manda na minha terra sou euzis! Detraxit consequat et quo num tendi nada. Suco de cevadiss deixa as pessoas mais interessantis."}
+	];
+	
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
